627a03ee0556c345c83031836e01954771d14b9a
Guo Sentao <nowandthen@qq.com>
2020-03-02 00:26:12 +0800

    create vs dev envirnment

--- example/example.run.sh
+++ example/example.run.sh
@@ -1,4 +1,4 @@
-
+﻿
 
 libcurl_dir=../lib/libcurl-7.56.0
 libcurl_lib=${libcurl_dir}/lib
@@ -12,4 +12,4 @@ libbinacpp_lib=${libbinacpp_dir}/lib
 export SSL_CERT_FILE=`pwd`/cacert.pem
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libcurl_lib:$libwebsockets_lib:$libbinacpp_lib
 
-./example $@
+#./example $@
